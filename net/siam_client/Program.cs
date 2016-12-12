309de2d17cefb498c88774762f7f7bd75f68f9c2
Danilo <id4ni10@gmail.com>
2016-12-12 18:54:47 -0300

    C# Client
    Cliente .NET para conexão com o ws.

---
+++ net/siam_client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace siam_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var xml_rps = File.ReadAllText(@"C:\caminho_para_meu_rps\rps.xml");
+
+                var certificate = new X509Certificate2(@"C:\caminho_para_meu_certificado\certificado.pfx", "password");
+
+                var client = new siam_service.NfseClient();
+
+                client.ClientCredentials.ClientCertificate.Certificate = certificate;
+
+                var result = client.ValidarXml(xml_rps); //<--  Validar XML
+
+                //var data = client.EnviarLoteRpsSincronoEnvio(xmlString); <--  Enviar
+                //var data = client.ConsultarLoteRpsEnvio(xmlString); <-- Consultar
+
+                Console.WriteLine(result.xmlString);
+            }
+            catch (Exception exp)
+            {
+                var exception_details = string.Format("{0}{1}{2}", exp.Message, Environment.NewLine, exp.StackTrace);
+
+                Console.WriteLine(exception_details);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
